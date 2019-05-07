e633f8fffada40b5cbc8936e0f0904401e13c692
bazzerk <ankur@bazzerk.com>
2019-05-07 20:24:05 +0000

    Generated code by SwaggerHub

--- aspnetcore/src/IO.Swagger/Controllers/DigitalGoodsApi.cs
+++ aspnetcore/src/IO.Swagger/Controllers/DigitalGoodsApi.cs
@@ -39,28 +39,35 @@ namespace IO.Swagger.Controllers
         /// <param name="costcoTrackingId">Unique ID to Correlate HTTP requests between a client and server.</param>
         /// <param name="lineItems">The collection of items to process</param>
         /// <param name="costcoEnv">Non-Production Environment Identifier.</param>
-        /// <response code="200">Success</response>
-        /// <response code="400">Bad Request</response>
-        /// <response code="500">Server Error</response>
+        /// <response code="200">Status 200</response>
+        /// <response code="400">Status 400</response>
+        /// <response code="500">Status 500</response>
         [HttpPut]
         [Route("/shipments/v1/fulfillment/digitalgoods/orders/{partnerOrderNumber}")]
         [ValidateModelState]
         [SwaggerOperation("FulfillDigitalGoods")]
-        [SwaggerResponse(statusCode: 400, type: typeof(ApiExceptionResponse), description: "Bad Request")]
-        [SwaggerResponse(statusCode: 500, type: typeof(ApiExceptionResponse), description: "Server Error")]
+        [SwaggerResponse(statusCode: 200, type: typeof(InlineResponse200), description: "Status 200")]
+        [SwaggerResponse(statusCode: 400, type: typeof(ApiExceptionResponse), description: "Status 400")]
+        [SwaggerResponse(statusCode: 500, type: typeof(GatewayError), description: "Status 500")]
         public virtual IActionResult FulfillDigitalGoods([FromRoute][Required]string partnerOrderNumber, [FromHeader][Required()]string costcoTrackingId, [FromBody]List<LineItem> lineItems, [FromHeader]string costcoEnv)
         { 
             //TODO: Uncomment the next line to return response 200 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
-            // return StatusCode(200);
+            // return StatusCode(200, default(InlineResponse200));
 
             //TODO: Uncomment the next line to return response 400 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
             // return StatusCode(400, default(ApiExceptionResponse));
 
             //TODO: Uncomment the next line to return response 500 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
-            // return StatusCode(500, default(ApiExceptionResponse));
+            // return StatusCode(500, default(GatewayError));
 
-
-            throw new NotImplementedException();
+            string exampleJson = null;
+            exampleJson = "{\n  \"result\" : \"ok\",\n  \"lineItems\" : [ {\n    \"itemNumber\" : 0.80082819046101150206595775671303272247314453125,\n    \"quantity\" : 6.02745618307040320615897144307382404804229736328125,\n    \"lineProcessingStatus\" : \"invalidItem\"\n  }, {\n    \"itemNumber\" : 0.80082819046101150206595775671303272247314453125,\n    \"quantity\" : 6.02745618307040320615897144307382404804229736328125,\n    \"lineProcessingStatus\" : \"invalidItem\"\n  } ]\n}";
+            
+            var example = exampleJson != null
+            ? JsonConvert.DeserializeObject<InlineResponse200>(exampleJson)
+            : default(InlineResponse200);
+            //TODO: Change the data returned
+            return new ObjectResult(example);
         }
     }
 }
